07fb061c00f70954d7d059959bebde582aea82b0
Mason <109998705+GreenMasonJar@users.noreply.github.com>
2024-02-24 19:12:51 -0700

    Pseudocode of functions needed for calc

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # pythonCalculator
-GUI calculator using Python and Tkintor
+GUI calculator using Python and Tkinter
+
+Welcome to my calculator! This is my first project using python and a gui. 
---
+++ calculator.py
@@ -0,0 +1,23 @@
+#import Tkinter
+
+##inputText is what goes into the calculator. As user inputs data this updates. 
+inputText = ""
+
+#Adds to inputText string
+def addToCalculation(symbol):
+    pass
+
+#converts string to calculation and does the math. Algebraic!
+#shows error if math is not mathing
+def evalCalc(inputText):
+    pass
+
+#When you hit the CA (Clear All) button it clears the calculator 
+def clearField():
+    pass
+
+#has a clear/delete button to delete one space
+def deleteButton():
+    pass
+
+#Add gui shiz here (I need to learn how Tkinter works...)
