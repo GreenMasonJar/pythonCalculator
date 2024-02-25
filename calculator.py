2f467633e4a499599420e979abff2f17a3fc3ece
Mason <109998705+GreenMasonJar@users.noreply.github.com>
2024-02-24 20:42:51 -0700

    Displayed buttons in tkinter GUI

--- calculator.py
+++ calculator.py
@@ -1,4 +1,4 @@
-#import Tkinter
+from tkinter import *
 
 ##inputText is what goes into the calculator. As user inputs data this updates. 
 inputText = ""
@@ -20,4 +20,33 @@ def clearField():
 def deleteButton():
     pass
 
-#Add gui shiz here (I need to learn how Tkinter works...)
+
+
+#GUI through Tkinter
+root = Tk()
+root.geometry("1000x1000")
+root.title("Calculator")
+
+#Creating grid of number buttons (sans 0)
+rowIndex = 0
+columnIndex = 0
+for i in range(1, 10):
+    if columnIndex > 2:
+        columnIndex = 0
+        rowIndex += 1
+    button = Button(root, text=i)
+    button.grid(row=rowIndex, column=columnIndex)
+    columnIndex+=1
+
+#creating special buttons AND 0 number
+special = ["(", "0", ")", "C", "CA", "="]
+for i in special:
+    if columnIndex > 2:
+        columnIndex = 0
+        rowIndex += 1
+
+    specialButton = Button(root, text=i)
+    specialButton.grid(row = rowIndex, column=columnIndex)
+    columnIndex+=1
+
+root.mainloop()
